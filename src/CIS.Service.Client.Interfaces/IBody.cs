56c2fd9ba201f9b45b28785afae5c256825eac33
Oleg Nadymov <OlegNadymov@gmail.com>
2022-07-15 16:38:58 +0300

    Removed the base lib from this repo

--- src/CIS.Service.Client.Interfaces/IBody.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CIS.Service.Client.Interfaces
-{
-    /// <summary>
-    /// Interface for the body of CIS.Service.Client.ContainerModel{T}.
-    /// </summary>
-    public interface IBody
-    {
-    }
-}
--- src/CIS.Service.Client.Interfaces/IConvertible.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CIS.Service.Client.Interfaces
-{
-    /// <summary>
-    /// The objects should be converterted by custom model converters.
-    /// </summary>
-    public interface IConvertible
-    {
-    }
-}
