9dc116fa44f2a26de58c54bcc7605aef42bb0e1d
Oleg Nadymov <OlegNadymov@gmail.com>
2022-09-26 14:06:33 +0300

    Country test

---
+++ src/CIS.Service.Client.ConsoleApp/Country.cs
@@ -0,0 +1,22 @@
+﻿using CIS.Service.Client.Interfaces;
+using Core.Common;
+
+namespace CIS.Service.Client.ConsoleApp
+{
+    public class Country : IdentityObject, IBody
+    {
+        public LocalizableString Name { get; set; }
+
+        /// <summary>
+        /// Код страны
+        /// </summary>
+        public int PhoneCode { get; set; }
+
+        /// <summary>
+        /// Код страны (бухгалтерский)
+        /// </summary>
+        public int AccCode { get; set; }
+
+        public string CountryCode { get; set; }
+    }
+}
